846d42f85d3a656e2b82dcfb01d0d65476ceee4a
Jairo Delgado <delgadojairo2014@gmail.com>
2019-08-10 11:41:51 -0500

    agrega carpeta con sql

---
+++ sql/secretaria.sql
@@ -0,0 +1,206 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-08-2019 a las 18:38:21
+-- Versión del servidor: 10.3.16-MariaDB
+-- Versión de PHP: 7.1.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `secretaria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `agente`
+--
+
+CREATE TABLE `agente` (
+  `idAgente` int(11) NOT NULL,
+  `nomAgente` varchar(50) COLLATE utf8_bin NOT NULL,
+  `Rango` varchar(50) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `agente`
+--
+
+INSERT INTO `agente` (`idAgente`, `nomAgente`, `Rango`) VALUES
+(1, 'Juan Perez ', 'Alto'),
+(2, 'Luis Luna', 'Bajo'),
+(3, 'jlulio gomez', '[object HTMLSelectElement]'),
+(4, 'mario moreno', '1'),
+(5, 'lusi paz ', 'undefined'),
+(6, 'onoria paz mora', 'undefined');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `conductor`
+--
+
+CREATE TABLE `conductor` (
+  `idConductor` int(11) NOT NULL,
+  `dni` varchar(50) COLLATE utf8_bin NOT NULL,
+  `nomConductor` varchar(50) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `conductor`
+--
+
+INSERT INTO `conductor` (`idConductor`, `dni`, `nomConductor`) VALUES
+(1, '1085000000', 'Luis Alberto Toro'),
+(2, '13072001', 'Jairo Delgado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `denuncia`
+--
+
+CREATE TABLE `denuncia` (
+  `idDenuncia` int(4) NOT NULL,
+  `idConductor` int(4) NOT NULL,
+  `idAgente` int(4) NOT NULL,
+  `idInfraccion` int(4) NOT NULL,
+  `fecha` date NOT NULL,
+  `pagada` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `denuncia`
+--
+
+INSERT INTO `denuncia` (`idDenuncia`, `idConductor`, `idAgente`, `idInfraccion`, `fecha`, `pagada`) VALUES
+(1, 1, 1, 1, '2019-08-05', 0),
+(2, 1, 1, 2, '2019-08-06', 0),
+(3, 1, 2, 2, '2019-08-18', 1),
+(4, 2, 2, 2, '2019-08-04', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `infraccion`
+--
+
+CREATE TABLE `infraccion` (
+  `idInfraccion` int(11) NOT NULL,
+  `descripcion` varchar(50) COLLATE utf8_bin NOT NULL,
+  `importe` bigint(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `infraccion`
+--
+
+INSERT INTO `infraccion` (`idInfraccion`, `descripcion`, `importe`) VALUES
+(1, 'Infraccion por Pico y Placa', 500000),
+(2, 'Cruce prohibido', 450000);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rango`
+--
+
+CREATE TABLE `rango` (
+  `idRango` int(11) NOT NULL,
+  `desRango` varchar(50) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `rango`
+--
+
+INSERT INTO `rango` (`idRango`, `desRango`) VALUES
+(1, 'Bajo'),
+(2, 'Medio'),
+(3, 'Alto');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `agente`
+--
+ALTER TABLE `agente`
+  ADD PRIMARY KEY (`idAgente`);
+
+--
+-- Indices de la tabla `conductor`
+--
+ALTER TABLE `conductor`
+  ADD PRIMARY KEY (`idConductor`);
+
+--
+-- Indices de la tabla `denuncia`
+--
+ALTER TABLE `denuncia`
+  ADD PRIMARY KEY (`idDenuncia`);
+
+--
+-- Indices de la tabla `infraccion`
+--
+ALTER TABLE `infraccion`
+  ADD PRIMARY KEY (`idInfraccion`);
+
+--
+-- Indices de la tabla `rango`
+--
+ALTER TABLE `rango`
+  ADD PRIMARY KEY (`idRango`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `agente`
+--
+ALTER TABLE `agente`
+  MODIFY `idAgente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `conductor`
+--
+ALTER TABLE `conductor`
+  MODIFY `idConductor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `denuncia`
+--
+ALTER TABLE `denuncia`
+  MODIFY `idDenuncia` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `infraccion`
+--
+ALTER TABLE `infraccion`
+  MODIFY `idInfraccion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `rango`
+--
+ALTER TABLE `rango`
+  MODIFY `idRango` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
